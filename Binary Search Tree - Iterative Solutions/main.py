1cf98f7a8e985d9a2efe82a2491c55516b2e5488
Hiru Wijemanne <hiruneshan@gmail.com>
2025-11-28 12:22:13 -0500

    iterative fun for floor

--- Binary Search Tree - Iterative Solutions/main.py
+++ Binary Search Tree - Iterative Solutions/main.py
@@ -14,3 +14,20 @@ def func(self, target):
     return self.value
 
 
+def find_floor(self, target):
+    if self.root is None:
+        return 0
+
+    curr = self.root
+    self.value = 0
+    while curr:
+        if curr.data < target:
+            self.value = curr.data
+            curr = curr.right
+
+        else:
+            curr = curr.left
+    return self.value
+
+
+
